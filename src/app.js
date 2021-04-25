9a62dc93e74c34e518514ce5fb204450a9d5b5b5
mryfdlg <81037778+mryfdlg@users.noreply.github.com>
2021-04-25 13:26:37 -0400

    Wind and humidity via API

--- src/app.js
+++ src/app.js
@@ -6,9 +6,13 @@ function displayTemperature(response) {
     temperatureDegrees.innerHTML = Math.round(response.data.main.temp);
     let weatherDesc = document.querySelector("#weather-description");
     weatherDesc.innerHTML = response.data.weather[0].description;
+    let humidityElement = document.querySelector("#humidity")
+    humidityElement.innerHTML = response.data.main.humidity;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
 }
   let apiKey = "fcdc838ee1dab066d3dcd7fb3d434327";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 
 let now = new Date();
